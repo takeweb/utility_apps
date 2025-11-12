7504dc9a4ac2acb04314ebd2e546abff6f99839c
Taketomo Oishi <oishi@multisoup.co.jp>
2025-11-12 14:38:03 +0900

    表示順を変更

--- pages/03_chart.py
+++ pages/03_chart.py
@@ -7,13 +7,13 @@ import tools.math.kaprekar as kaprekar
 
 def main():
     st.title("数学グラフアプリ")
-    fibonacci.gen_chart()
     sin.gen_sine_chart(1000)
     sin.gen_arcsine_chart(1000)
     cos.gen_cosine_chart(1000)
     cos.gen_arccosine_chart(1000)
     tan.gen_tangent_chart()
     tan.gen_arctangent_chart()
+    fibonacci.gen_chart()
     kaprekar.gen_chart()
 
 if __name__ == "__main__":
